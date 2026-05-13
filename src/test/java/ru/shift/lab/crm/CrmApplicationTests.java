d472f1d136e5579e8b2a0031f5175d9975309436
colder159 <qwezitrakspsiblade@gmail.com>
2026-05-13 14:52:05 +0700

    Initial commit

---
+++ src/test/java/ru/shift/lab/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shift.lab.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
